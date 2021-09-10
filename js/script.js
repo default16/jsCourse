6331e9f530e4b9a4f08fe210d388061d70f4b93e
Rasl <examble@.com>
2021-09-11 00:19:14 +0600

    Добавил условия и циклы

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-"use strict";
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
 
 // 1 +
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+if (numberOfFilms < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+  alert('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
+}
 
 // 2 +
 const personalMovieDB = {
@@ -14,11 +40,17 @@ const personalMovieDB = {
 
 // 3 +
 
-const lastMovieSeen = prompt("Один из последних просмотренных фильмов?", "");
-const rating = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const lastMovieSeen = prompt("Один из последних просмотренных фильмов?", "");
+  const rating = prompt("На сколько оцените его?", "");
 
-const lastMovieSeen2 = prompt("Один из последних просмотренных фильмов?", "");
-const rating2 = prompt("На сколько оцените его?", "");
+  if (lastMovieSeen != null && rating != null && lastMovieSeen != '' && rating != '' && lastMovieSeen.length < 50) {
+    personalMovieDB.movies[lastMovieSeen] = rating;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
 
-personalMovieDB.movies[lastMovieSeen] = rating;
-personalMovieDB.movies[lastMovieSeen2] = rating2;
+console.log(personalMovieDB);
